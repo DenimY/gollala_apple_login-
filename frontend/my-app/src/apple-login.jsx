11ff03ba14a52372366487e11c6c37cd23f9ce3e
youkyungmu <ykm3976@gmail.com>
2021-04-04 01:06:20 +0900

    init

---
+++ frontend/my-app/src/apple-login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppleLogin = async () => {
+
+    return (
+        <div>
+            <h2>Apple Login Demo</h2>
+        </div>
+    )
+
+}
+export default AppleLogin
